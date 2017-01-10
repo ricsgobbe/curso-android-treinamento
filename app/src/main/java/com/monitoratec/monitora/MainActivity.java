788d52f2fdade1a7b010de6e2c843cda3a49dc83
ricardo.sgobbe <Ricardo.Sgobbe@vistajet.com>
2017-01-10 19:41:47 -0200

    adding layout at main screen

--- app/src/main/java/com/monitoratec/monitora/MainActivity.java
+++ app/src/main/java/com/monitoratec/monitora/MainActivity.java
@@ -1,11 +1,17 @@
 package com.monitoratec.monitora;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.monitoratec.monitora.domain.entity.GithubStatusApi;
 import com.monitoratec.monitora.domain.entity.Status;
+
+import java.io.IOException;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -15,10 +21,16 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private TextView gitText;
+    private ImageView gitImage;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        gitText = (TextView) findViewById(R.id.git_text_status);
+        gitImage = (ImageView) findViewById(R.id.git_image);
 
         final Retrofit retrofit = new Retrofit.Builder()
                 .addConverterFactory(GsonConverterFactory.create())
@@ -31,8 +43,28 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onResponse(Call<Status> call, Response<Status> response) {
                 if(response.isSuccessful()){
-                    Status body = response.body();
-                    Toast.makeText(MainActivity.this, body.status, Toast.LENGTH_LONG).show();
+                    Status.Type status = response.body().status;
+                    switch (status){
+                        case GOOD:
+                            setImageAndTextColor(status);
+                            break;
+                        case MINOR:
+                            setImageAndTextColor(status);
+                            break;
+                        case MAJOR:
+                            setImageAndTextColor(status);
+                            break;
+                    }
+
+                /*    Status body = response.body();
+                    Toast.makeText(MainActivity.this, body.status, Toast.LENGTH_LONG).show();*/
+                }else{
+                    try {
+                        String errorBody = response.errorBody().string();
+                        Toast.makeText(MainActivity.this, errorBody, Toast.LENGTH_LONG).show();
+                    } catch (IOException e) {
+                        Log.e(TAG, e.getMessage(), e);
+                    }
                 }
             }
 
@@ -41,5 +73,14 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+
+    private void setImageAndTextColor(Status.Type status){
+                int color = getResources().getColor(status.getColor());
+                gitText.setText(status.getStatusName());
+                gitText.setTextColor(color);
+                gitImage.setBackgroundColor(color);
+
     }
 }
--- app/src/main/java/com/monitoratec/monitora/domain/entity/Status.java
+++ app/src/main/java/com/monitoratec/monitora/domain/entity/Status.java
@@ -1,16 +1,43 @@
 package com.monitoratec.monitora.domain.entity;
 
+import com.google.gson.annotations.SerializedName;
+import com.monitoratec.monitora.R;
+
 /**
  * Created by ricardo.sgobbe on 09/01/2017.
  */
 
 public class Status {
 
-    public String status;
+    public Type  status;
     public String body;
     public String created_on;
 
 
+    public enum Type{
+        @SerializedName("good")
+        GOOD(R.color.blue, "good"),
+        @SerializedName("minor")
+        MINOR(R.color.orange, "minor"),
+        @SerializedName("major")
+        MAJOR(R.color.red, "major");
+
+        int color;
+        String statusName;
+
+        Type(int color, String statusName){
+            this.color = color;
+            this.statusName = statusName;
+        }
+
+        public int getColor(){
+            return color;
+        }
+
+        public String getStatusName(){
+            return statusName;
+        }
+    }
 
 
 }
